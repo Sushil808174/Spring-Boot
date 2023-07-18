b94e543ac780bbd41b9f2b5a000fd99e89647249
Susheel Kumar <sushilkumar8081742575@gmail.com>
2023-07-19 00:04:34 +0530

    update

---
+++ Restfull-web-services/src/main/java/com/skumar/restfull/webservices/RestfullWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.skumar.restfull.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullWebServicesApplication.class, args);
+	}
+
+}
