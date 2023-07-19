48ea6871a49340c30ff025ad2b50631cfb114469
Susheel Kumar <sushilkumar8081742575@gmail.com>
2023-07-20 02:35:57 +0530

    updated

--- ReactApp/todo-app/src/App.js
+++ ReactApp/todo-app/src/App.js
@@ -1,33 +1,18 @@
 import './App.css';
-import {Component} from 'react'
+import FirstComponent from './components/learn-example/FirstComponent';
+import SecondComponent from './components/learn-example/SecondComponent';
+import ThirdComponent from './components/learn-example/ThirdComponent';
+
 function App() {
   return (
     <div className="App">
-      <FirstComponent></FirstComponent>
-      <SecondComponent></SecondComponent>
-      <ThirdComponent></ThirdComponent>
+      <FirstComponent/>
+      <SecondComponent/>
+      <ThirdComponent/>
     </div>
   );
 }
 
-function FirstComponent() {
-  return (
-    <div className="FirstComponent"> This is my first component.</div>
-  );
-}
 
-function SecondComponent() {
-  return (
-    <div className="SecondComponent"> This is my second component.</div>
-  );
-}
-
-class ThirdComponent extends Component {
-  render() {
-    return (
-      <div className="ThirdComponent"> This is my Third component.</div>
-    );
-  }
-}
 
 export default App;
---
+++ ReactApp/todo-app/src/components/learn-example/SecondComponent.jsx
@@ -0,0 +1,5 @@
+export default function SecondComponent() {
+  return (
+    <div className="SecondComponent"> This is my second component.</div>
+  );
+}
