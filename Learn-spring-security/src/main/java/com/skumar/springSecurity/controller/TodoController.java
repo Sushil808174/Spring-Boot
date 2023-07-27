ab8be36353e1c1c28a62b1017dedeec52c864d88
Susheel Kumar <sushilkumar8081742575@gmail.com>
2023-07-28 00:23:35 +0530

    updated

--- Learn-spring-security/src/main/java/com/skumar/springSecurity/controller/TodoController.java
+++ Learn-spring-security/src/main/java/com/skumar/springSecurity/controller/TodoController.java
@@ -3,14 +3,22 @@ package com.skumar.springSecurity.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
 
+	private static final List<Todo> TODOS_LIST = List.of(new Todo("Susheel","Learn Python"),new Todo("Susheel","Learn AWS"));
+
 	@GetMapping("/todos")
 	public List<Todo> retrieveAllTodo(){
-		return List.of(new Todo("Susheel","Learn Python"),new Todo("Susheel","Learn AWS"));
+		return TODOS_LIST;
+	}
+	@GetMapping("/users/{username}/todos")
+	public Todo retrievePerticularTodo(@PathVariable String username){
+		return TODOS_LIST.get(0);
 	}
 }
 record Todo (String username,String description) {}
