4cbab9ddbed8f61e3bd8fa30ae338796f7b6a596
Susheel Kumar <sushilkumar8081742575@gmail.com>
2023-07-31 01:21:22 +0530

    google oauth added

---
+++ Learn-Oauth2/src/main/java/com/skumar/springSecurity/LearnOauth2Application.java
@@ -0,0 +1,13 @@
+package com.skumar.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOauth2Application.class, args);
+	}
+
+}
---
+++ Learn-Oauth2/src/test/java/com/skumar/springSecurity/LearnOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skumar.springSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
